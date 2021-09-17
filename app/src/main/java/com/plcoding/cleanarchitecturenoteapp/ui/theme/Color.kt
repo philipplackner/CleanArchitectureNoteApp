baeb9dd2dcdcf629d44e22ea5f76a5ce5502c104
Philipp <mail@pl-coding.com>
2021-09-17 21:16:33 +0200

    Initial commit

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.plcoding.cleanarchitecturenoteapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkGray = Color(0xFF202020)
+val LightBlue = Color(0xFFD7E8DE)
+
+val RedOrange = Color(0xffffab91)
+val RedPink = Color(0xfff48fb1)
+val BabyBlue = Color(0xff81deea)
+val Violet = Color(0xffcf94da)
+val LightGreen = Color(0xffe7ed9b)
---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/ui/theme/Theme.kt
@@ -0,0 +1,24 @@
+package com.plcoding.cleanarchitecturenoteapp.ui.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = Color.White,
+    background = DarkGray,
+    onBackground = Color.White,
+    surface = LightBlue,
+    onSurface = DarkGray
+)
+
+@Composable
+fun CleanArchitectureNoteAppTheme(darkTheme: Boolean = true, content: @Composable() () -> Unit) {
+    MaterialTheme(
+        colors = DarkColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
